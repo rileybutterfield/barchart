ed43a7d7f15bdcb7e2631a033269e8249f01c6e1
Riley Butterfield <rileybutterfield1@gmail.com>
2022-01-05 13:53:26 -0800

    add interval to switch

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -1,13 +1,13 @@
 import * as d3 from "d3";
 
-const url = "https://udemy-react-d3.firebaseio.com/tallest_men.json";
 const MARGIN = { TOP: 10, BOTTOM: 50, LEFT: 70, RIGHT: 10 };
 const WIDTH = 800 - MARGIN.LEFT - MARGIN.RIGHT;
 const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM;
 
 export default class D3Chart {
   constructor(element) {
-    const svg = d3
+    const vis = this;
+    vis.svg = d3
       .select(element)
       .append("svg")
       .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
@@ -15,53 +15,84 @@ export default class D3Chart {
       .append("g")
       .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`);
 
-    d3.json(url).then((data) => {
-      const y = d3
-        .scaleLinear()
-        .domain([
-          d3.min(data, (d) => d.height) * 0.95,
-          d3.max(data, (d) => d.height),
-        ])
-        .range([HEIGHT, 0]);
-      const x = d3
-        .scaleBand()
-        .domain(data.map((d) => d.name))
-        .range([0, WIDTH])
-        .padding(0.4);
-      const rects = svg.selectAll("rect").data(data);
+    vis.svg
+      .append("text")
+      .attr("x", WIDTH / 2)
+      .attr("y", HEIGHT + 50)
+      .attr("text-anchor", "middle")
+      .text("The world's tallest men");
 
-      const xAxisCall = d3.axisBottom(x);
-      svg
-        .append("g")
-        .attr("transform", `translate(0, ${HEIGHT})`)
-        .call(xAxisCall);
+    vis.svg
+      .append("text")
+      .attr("x", -HEIGHT / 2)
+      .attr("y", -50)
+      .attr("text-anchor", "middle")
+      .text("Height in cm")
+      .attr("transform", "rotate(-90)");
 
-      const yAxisCall = d3.axisLeft(y);
-      svg.append("g").call(yAxisCall);
+    vis.xAxisGroup = vis.svg
+      .append("g")
+      .attr("transform", `translate(0, ${HEIGHT})`);
 
-      svg
-        .append("text")
-        .attr("x", WIDTH / 2)
-        .attr("y", HEIGHT + 50)
-        .attr("text-anchor", "middle")
-        .text("The world's tallest men");
+    vis.yAxisGroup = vis.svg.append("g");
 
-      svg
-        .append("text")
-        .attr("x", -HEIGHT / 2)
-        .attr("y", -50)
-        .attr("text-anchor", "middle")
-        .text("Height in cm")
-        .attr("transform", "rotate(-90)");
-
-      rects
-        .enter()
-        .append("rect")
-        .attr("x", (d) => x(d.name))
-        .attr("y", (d) => y(d.height))
-        .attr("width", x.bandwidth)
-        .attr("height", (d) => HEIGHT - y(d.height))
-        .attr("fill", "hotpink");
+    Promise.all([
+      d3.json("https://udemy-react-d3.firebaseio.com/tallest_men.json"),
+      d3.json("https://udemy-react-d3.firebaseio.com/tallest_women.json"),
+    ]).then((datasets) => {
+      const [men, women] = datasets;
+      let flag = true;
+      vis.data = men;
+      vis.update();
+      d3.interval(() => {
+        vis.data = flag ? men : women;
+        vis.update();
+        flag = !flag;
+      }, 1000);
     });
+  }
+  update() {
+    const vis = this;
+    const y = d3
+      .scaleLinear()
+      .domain([
+        d3.min(vis.data, (d) => d.height) * 0.95,
+        d3.max(vis.data, (d) => d.height),
+      ])
+      .range([HEIGHT, 0]);
+    const x = d3
+      .scaleBand()
+      .domain(vis.data.map((d) => d.name))
+      .range([0, WIDTH])
+      .padding(0.4);
+
+    const xAxisCall = d3.axisBottom(x);
+    vis.xAxisGroup.call(xAxisCall);
+
+    const yAxisCall = d3.axisLeft(y);
+    vis.yAxisGroup.call(yAxisCall);
+
+    //DATA JOIN
+    const rects = vis.svg.selectAll("rect").data(vis.data);
+
+    //EXIT
+    rects.exit().remove();
+
+    //UPDATE
+    rects
+      .attr("x", (d) => x(d.name))
+      .attr("y", (d) => y(d.height))
+      .attr("width", x.bandwidth)
+      .attr("height", (d) => HEIGHT - y(d.height));
+
+    //ENTER
+    rects
+      .enter()
+      .append("rect")
+      .attr("x", (d) => x(d.name))
+      .attr("y", (d) => y(d.height))
+      .attr("width", x.bandwidth)
+      .attr("height", (d) => HEIGHT - y(d.height))
+      .attr("fill", "hotpink");
   }
 }
